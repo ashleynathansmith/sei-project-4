41543e16002ea0e4f23e0c1d470f0b6fcaa0d746
Ash Smith <ashleynathansmith@gmail.com>
2021-02-04 14:26:02 +0000

    ready to deploy

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path 
+from .views import index 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/articles/', include('articles.urls')),
     path('api/comments/', include('comments.urls')),
     path('api/types/', include('article_types.urls')),
-    path('api/auth/', include('jwt_auth.urls'))
+    path('api/auth/', include('jwt_auth.urls')),
+    re_path(r'^.*$', index)
 ]
